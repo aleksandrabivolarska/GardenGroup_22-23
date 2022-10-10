19d0d7c346fc4f53be5d8fe263c3c80c729b8975
YvanRoes <90270910+YvanRoes@users.noreply.github.com>
2022-10-10 14:30:21 +0200

    init project
    init project data commit

---
+++ DAL/MongoHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace DAL
+{
+    public class MongoHelper
+    {
+        protected static MongoClient _client;
+        protected static MongoHelper _helper; 
+
+        private MongoHelper() { _client = new MongoClient("mongodb+srv://gg3:gg3@cluster0.mhym582.mongodb.net/?retryWrites=true&w=majority"); }
+
+        public static MongoHelper getInstance() 
+        {
+            if (_helper == null)
+                _helper = new MongoHelper();
+            return _helper;
+        }
+
+        public List<MongoDB.Bson.BsonDocument> getAllDbs()
+        {
+            List<MongoDB.Bson.BsonDocument> db = _client.ListDatabases().ToList();
+            return db;
+        }
+
+    }
+}
---
+++ UI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAL;
+using MongoDB.Driver;
+
+namespace UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        void Start()
+        {
+            MongoHelper mongodb = MongoHelper.getInstance();
+            var dbs = mongodb.getAllDbs();
+            dbs.ForEach(d => listBox1.Items.Add(d));
+        }
+    }
+}
